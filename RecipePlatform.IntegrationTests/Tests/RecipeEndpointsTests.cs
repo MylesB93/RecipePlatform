cb0f2d6970aca1d820b6882415196f6a064a4c7e
Myles Broomes <mylesbroomes@hotmail.co.uk>
2026-07-24 16:45:47 +0100

    WIP: add tests for invalid get, invalid post and delete

--- RecipePlatform.IntegrationTests/Tests/RecipeEndpointsTests.cs
+++ RecipePlatform.IntegrationTests/Tests/RecipeEndpointsTests.cs
@@ -66,4 +66,55 @@ public sealed class RecipeEndpointsTests
 			request.Description,
 			retrievedRecipe.Description);
 	}
+
+	[Fact]
+	public async Task GetRecipe_WithInvalidRequest_Returns404()
+	{
+		// Arrange
+		var InvalidId = "unknownId";
+
+		// Act
+		HttpResponseMessage responseMessage =
+			await _client.GetAsync(
+				$"/api/recipes/{InvalidId}");
+
+		// Assert
+		Assert.Equal(HttpStatusCode.NotFound, responseMessage.StatusCode);
+	}
+
+	[Fact]
+	public async Task PostRecipe_WithNoName_Returns400()
+	{
+		// Arrange
+		var request = new CreateRecipeRequest("", "Invalid recipe");
+
+		// Act
+		var response = await _client.PostAsJsonAsync($"/api/recipes", request);
+
+		// Assert
+		Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+	}
+
+	[Fact]
+	public async Task DeleteRecipe_WithValidRequest_RemovesRecipeAndReturns204()
+	{
+		// Arrange
+		var createRequest = new CreateRecipeRequest("Pancakes", "Some delicious pancakes");
+
+		// Act create response
+		var createResponse = await _client.PostAsJsonAsync($"/api/recipes", createRequest);
+
+		// Assert response is valid
+		Assert.NotNull(createResponse);
+		Assert.Equal(HttpStatusCode.Created, createResponse.StatusCode);
+
+		// Arrange 
+		var deletedRecipe = await createResponse.Content.ReadFromJsonAsync<RecipeResponse>();
+
+		// Act delete recipe
+		var deletedResponse = await _client.DeleteAsync($"/api/recipes/{deletedRecipe?.Id}");
+
+		// Assert
+		Assert.Equal(HttpStatusCode.NoContent, deletedResponse.StatusCode);
+	}
 }
